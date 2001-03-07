48653c260d09c70bd460f7fc172e15326431d8da
mohrt <mohrt@localhost>
2001-03-07 04:52:27 +0000

    update everything to 1.3.1

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -5,7 +5,7 @@
  * Author:      Monte Ohrt <monte@ispi.net>
  *              Andrei Zmievski <andrei@ispi.net>
  *
- * Version:             1.3.0
+ * Version:             1.3.1
  * Copyright:           2001 ispi of Lincoln, Inc.
  *              
  * This library is free software; you can redistribute it and/or
