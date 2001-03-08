0892a299ae22ed7faae7e492ac35bdf61dbad710
mohrt <mohrt@localhost>
2001-03-08 20:34:30 +0000

    rename 1.3.1b to 1.3.1pl1

--- Smarty_Compiler.class.php
+++ Smarty_Compiler.class.php
@@ -6,7 +6,7 @@
  * Author:      Monte Ohrt <monte@ispi.net>
  *              Andrei Zmievski <andrei@ispi.net>
  *
- * Version:             1.3.1b
+ * Version:             1.3.1pl1
  * Copyright:           2001 ispi of Lincoln, Inc.
  *
  * This library is free software; you can redistribute it and/or
