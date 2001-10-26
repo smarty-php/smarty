4190a446aa04eca72628b4fc2551fcf636601e0d
mohrt <mohrt@localhost>
2001-10-26 15:30:01 +0000

    fix debug console error

--- Smarty.addons.php
+++ Smarty.addons.php
@@ -684,22 +684,22 @@ function smarty_func_counter() {
     Purpose:  assign debug info to the template
 \*======================================================================*/
 function smarty_func_assign_debug_info($args, &$smarty_obj) {
-    $assigned_vars = $smarty_obj->_tpl_vars;
-    ksort($assigned_vars);
-    if (is_array($smarty_obj->_config[0])) {
-        $config_vars = $smarty_obj->_config[0];
-        ksort($config_vars);
-        $smarty_obj->assign("_debug_config_keys",array_keys($config_vars));
-        $smarty_obj->assign("_debug_config_vals",array_values($config_vars));
-    }
-
-    $included_templates = $smarty_obj->_included_tpls;
-
-    $smarty_obj->assign("_debug_keys",array_keys($assigned_vars));
-    $smarty_obj->assign("_debug_vals",array_values($assigned_vars));
-
-    $smarty_obj->assign("_debug_tpls",$included_templates);
-    return true;
+	$assigned_vars = $smarty_obj->_tpl_vars;
+	ksort($assigned_vars);
+	if(is_array($smarty_obj->_config[0])) {
+		$config_vars = $smarty_obj->_config[0];
+		ksort($config_vars);
+		$smarty_obj->assign("_debug_config_keys",array_keys($config_vars));
+		$smarty_obj->assign("_debug_config_vals",array_values($config_vars));
+	}	
+	
+	$included_templates = $smarty_obj->_smarty_debug_info;
+	
+	$smarty_obj->assign("_debug_keys",array_keys($assigned_vars));
+	$smarty_obj->assign("_debug_vals",array_values($assigned_vars));
+	
+	$smarty_obj->assign("_debug_tpls",$included_templates);
+	return true;
 }
 
 /*======================================================================*\
