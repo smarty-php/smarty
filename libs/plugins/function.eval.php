e3556e0c0b6b4bd4a8a52bb4c5b12dff6906920c
mohrt <mohrt@localhost>
2002-02-27 22:30:05 +0000

    added eval function to plugin dir

---
+++ libs/plugins/function.eval.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * Type:     function
+ * Name:     eval
+ * Purpose:  evaluate a template variable as a template
+ * -------------------------------------------------------------
+ */
+function smarty_function_eval($params, &$this)
+{
+    extract($params);
+
+    if (empty($var)) {
+        $this->trigger_error("assign: missing 'var' parameter");
+        return;
+    }
+
+	$this->_compile_template("eval_var",$var,$source);
+	
+    if (!empty($assign)) {
+        ob_start();
+		eval('?>'.$source);
+        $this->assign($assign,ob_get_contents());
+        ob_end_clean();
+    } else {
+		eval('?>'.$source);
+    }
+}
+
+/* vim: set expandtab: */
+
+?>
