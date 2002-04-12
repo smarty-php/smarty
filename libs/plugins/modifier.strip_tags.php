e693afbecaa43fa0b0700b54cf8ba550017c101a
mohrt <mohrt@localhost>
2002-04-12 14:48:48 +0000

    back out changes to strip_tags

--- libs/plugins/modifier.strip_tags.php
+++ libs/plugins/modifier.strip_tags.php
@@ -8,12 +8,12 @@
  * Purpose:  strip html tags from text
  * -------------------------------------------------------------
  */
-function smarty_modifier_strip_tags($string, $replace_with_space = true, $allowed = "<i><b><br>")
+function smarty_modifier_strip_tags($string, $replace_with_space = true)
 {
     if ($replace_with_space)
         return preg_replace('!<[^>]*?>!', ' ', $string);
     else
-        return strip_tags($string, $allowed);
+        return strip_tags($string);
 }
 
 /* vim: set expandtab: */
