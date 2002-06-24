3469b357f9961a957103fd1a308d5a3bc13e00ff
mohrt <mohrt@localhost>
2002-06-24 20:22:15 +0000

    fixed warning message in function.assign_debug_info

--- libs/plugins/function.assign_debug_info.php
+++ libs/plugins/function.assign_debug_info.php
@@ -12,7 +12,7 @@ function smarty_function_assign_debug_info($params, &$smarty)
 {
     $assigned_vars = $smarty->_tpl_vars;
     ksort($assigned_vars);
-    if (is_array($smarty->_config[0])) {
+    if (@is_array($smarty->_config[0])) {
         $config_vars = $smarty->_config[0];
         ksort($config_vars);
         $smarty->assign("_debug_config_keys", array_keys($config_vars));
