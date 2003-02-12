77fa84916e042fe48b79b82be1bb159238988454
mohrt <mohrt@localhost>
2003-02-12 22:13:18 +0000

    fix bug with decimal numbers in if statements, misc doc updates

--- libs/Smarty_Compiler.class.php
+++ libs/Smarty_Compiler.class.php
@@ -1097,7 +1097,7 @@ class Smarty_Compiler extends Smarty {
         preg_match_all('/(?>
 				' . $this->_obj_call_regexp . '(?:' . $this->_mod_regexp . '*) | # valid object call
 				' . $this->_var_regexp . '(?:' . $this->_mod_regexp . '*)	| # var or quoted string
-				\-?\d+|!==|<=>|==|!=|<=|>=|\&\&|\|\||\(|\)|,|\!|\^|=|<|>|\||\%|\+|\-|\/|\*	| # valid non-word token
+				\-?\d+(?:\.\d+)?|!==|<=>|==|!=|<=|>=|\&\&|\|\||\(|\)|,|\!|\^|=|<|>|\||\%|\+|\-|\/|\*	| # valid non-word token
 				\b\w+\b														| # valid word token
 				\S+                                                           # anything else
 				)/x', $tag_args, $match);
