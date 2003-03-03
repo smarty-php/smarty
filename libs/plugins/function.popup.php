3f4f215ce2d2ad78c42defeb3299635bdca92ad5
mohrt <mohrt@localhost>
2003-03-03 20:43:29 +0000

    fixed PHP notice

--- libs/plugins/function.popup.php
+++ libs/plugins/function.popup.php
@@ -19,7 +19,7 @@ function smarty_function_popup($params, &$smarty)
 
     if (empty($trigger)) { $trigger = "onMouseOver"; }
 
-    $retval .= $trigger . '="return overlib(\''.preg_replace(array("!'!","![\r\n]!"),array("\'",'\r'),$text).'\'';
+    $retval = $trigger . '="return overlib(\''.preg_replace(array("!'!","![\r\n]!"),array("\'",'\r'),$text).'\'';
     if ($sticky) { $retval .= ",STICKY"; }
     if (!empty($caption)) { $retval .= ",CAPTION,'".str_replace("'","\'",$caption)."'"; }
     if (!empty($fgcolor)) { $retval .= ",FGCOLOR,'$fgcolor'"; }
