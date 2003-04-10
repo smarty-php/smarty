e09d3070a18d91ce78171e717960ebe57b76f030
mohrt <mohrt@localhost>
2003-04-10 20:55:36 +0000

    fixed bug with default modifier when passing integer 0

--- libs/plugins/modifier.default.php
+++ libs/plugins/modifier.default.php
@@ -10,7 +10,7 @@
  */
 function smarty_modifier_default($string, $default = '')
 {
-    if (!isset($string) || $string == '')
+    if (!isset($string) || $string === '')
         return $default;
     else
         return $string;
