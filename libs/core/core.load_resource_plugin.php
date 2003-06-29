0d2409b7aa8a8acf23af9b3813c5eaf5a91a4c83
messju <messju@localhost>
2003-06-29 12:25:03 +0000

    fixed inlude_once-call

--- libs/core/core.load_resource_plugin.php
+++ libs/core/core.load_resource_plugin.php
@@ -48,7 +48,7 @@ function smarty_core_load_resource_plugin($params, &$smarty)
          * If the plugin file is found, it -must- provide the properly named
          * plugin functions.
          */
-        $smarty->smarty_include_once($_plugin_file);
+        include_once($_plugin_file);
 
         /*
          * Locate functions that we require the plugin to provide.
