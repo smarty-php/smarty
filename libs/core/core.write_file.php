38ed839cce2375fd998597a0c8809af48a7229e5
messju <messju@localhost>
2003-06-30 15:48:26 +0000

    fixed indentiation, silenced occasional warning

--- libs/core/core.write_file.php
+++ libs/core/core.write_file.php
@@ -15,17 +15,17 @@
  */    
 function smarty_core_write_file($params, &$smarty)
 {
-	$_dirname = dirname($params['filename']);
+    $_dirname = dirname($params['filename']);
 
     if ($params['create_dirs']) {
-		$_params = array('dir' => $_dirname);
-		require_once(SMARTY_DIR . 'core' . DIRECTORY_SEPARATOR . 'core.create_dir_structure.php');
-		smarty_core_create_dir_structure($_params, $smarty);
-	}
+        $_params = array('dir' => $_dirname);
+        require_once(SMARTY_DIR . 'core' . DIRECTORY_SEPARATOR . 'core.create_dir_structure.php');
+        smarty_core_create_dir_structure($_params, $smarty);
+    }
 
-	// write to tmp file, then rename it to avoid
-	// file locking race condition
-	$_tmp_file = $_dirname . DIRECTORY_SEPARATOR . uniqid('');
+    // write to tmp file, then rename it to avoid
+    // file locking race condition
+    $_tmp_file = $_dirname . DIRECTORY_SEPARATOR . uniqid('');
 
     if (!($fd = @fopen($_tmp_file, 'w'))) {
         $smarty->trigger_error("problem writing temporary file '$_tmp_file'");
@@ -34,10 +34,10 @@ function smarty_core_write_file($params, &$smarty)
 
     fwrite($fd, $params['contents']);
     fclose($fd);
-	if(file_exists($params['filename'])) {
-		@unlink($params['filename']);
-	} 
-	@rename($_tmp_file, $params['filename']);
+    if(file_exists($params['filename'])) {
+        @unlink($params['filename']);
+    } 
+    @rename($_tmp_file, $params['filename']);
     @chmod($params['filename'], $smarty->_file_perms);
 
     return true;
