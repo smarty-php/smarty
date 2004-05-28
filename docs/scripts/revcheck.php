2d20e3067c1e35296368153b4e93f81f1bc8512f
nlopess <nlopess@localhost>
2004-05-28 21:30:11 +0000

    changed title #revcheck now on-line (daily generated) at: http://phpdocmanager.sf.net/revcheck-smarty/

--- docs/scripts/revcheck.php
+++ docs/scripts/revcheck.php
@@ -531,7 +531,7 @@ print <<<END_OF_MULTILINE
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd">
 <html>
 <head>
-<title>PHPDOC Revision-check</title>
+<title>Smarty Manual Revision-check</title>
 <meta http-equiv="Content-Type" content="text/html; charset={$charset}">
 <style type="text/css">
 <!--
