b67e942fda8086d20e46ed1a8fd5dd5156c7efb0
mohrt <mohrt@localhost>
2004-06-23 13:56:10 +0000

    display date_format %e, %T and %D as expected for windows

--- libs/plugins/modifier.date_format.php
+++ libs/plugins/modifier.date_format.php
@@ -29,6 +29,11 @@ require_once $smarty->_get_plugin_filepath('shared','make_timestamp');
  */
 function smarty_modifier_date_format($string, $format="%b %e, %Y", $default_date=null)
 {
+    if (substr(PHP_OS,0,3) == 'WIN') {
+           $_win_from = array ('%e',  '%T',       '%D');
+           $_win_to   = array ('%#d', '%H:%M:%S', '%m/%d/%y');
+           $format = str_replace($_win_from, $_win_to, $format);
+    }
     if($string != '') {
         return strftime($format, smarty_make_timestamp($string));
     } elseif (isset($default_date) && $default_date != '') {
