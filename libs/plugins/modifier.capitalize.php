46b8afae2a93307ba989e23ae1a7a2353ba407d3
mohrt <mohrt@localhost>
2004-08-23 21:08:08 +0000

    implement optional param to capitalize for digit behavior

--- libs/plugins/modifier.capitalize.php
+++ libs/plugins/modifier.capitalize.php
@@ -17,14 +17,15 @@
  * @param string
  * @return string
  */
-function smarty_modifier_capitalize($string)
+function smarty_modifier_capitalize($string, $uc_digits = false)
 {
-    return preg_replace_callback('!\b\w+\b!', 'smarty_modifier_capitalize_ucfirst', $string);    
+    define('SMARTY_CAPITALIZE_UCDIGIT', $uc_digits);
+    return preg_replace_callback('!\b\w+\b!', 'smarty_modifier_capitalize_ucfirst', $string);
 }
 
 function smarty_modifier_capitalize_ucfirst($string)
 {
-    if(!preg_match('!\d!',$string[0]))
+    if(!preg_match('!\d!',$string[0]) || SMARTY_CAPITALIZE_UCDIGIT)
         return ucfirst($string[0]);
     else
         return $string[0];
