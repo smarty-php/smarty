dc726ed4ff531cb9c15961fd10d0036ac939f4ef
messju <messju@localhost>
2004-09-11 18:52:13 +0000

    removed notice from {debug} thanks to Peter Billen for pointing this one out!

--- libs/plugins/function.debug.php
+++ libs/plugins/function.debug.php
@@ -23,8 +23,8 @@
  */
 function smarty_function_debug($params, &$smarty)
 {
-    if($params['output']) {
-        $smarty->assign('_smarty_debug_output',$params['output']);
+    if (isset($params['output'])) {
+        $smarty->assign('_smarty_debug_output', $params['output']);
     }
     require_once(SMARTY_DIR . 'core' . DIRECTORY_SEPARATOR . 'core.display_debug_console.php');
     return smarty_core_display_debug_console(null, $smarty);
