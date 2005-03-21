3c8c2731781bdf5db601fd5489894c265249df2f
mohrt <mohrt@localhost>
2005-03-21 14:09:50 +0000

    remove e-modifier

--- libs/plugins/modifier.regex_replace.php
+++ libs/plugins/modifier.regex_replace.php
@@ -21,6 +21,10 @@
  */
 function smarty_modifier_regex_replace($string, $search, $replace)
 {
+    if (preg_match('!\W(\w+)$!s', $search, $match) && (strpos($match[1], 'e') !== false)) {
+        /* remove eval-modifier from $search */
+        $search = substr($search, 0, -strlen($match[1])) . str_replace('e', '', $match[1]);
+    }
     return preg_replace($search, $replace, $string);
 }
 
