08daa7ba84a97f41cf64632563f5eb1370ea4b09
boots <boots@localhost>
2006-05-09 19:48:54 +0000

    reverted {popup_init} as proposed change to insertion behviour was not BC

--- libs/plugins/function.popup_init.php
+++ libs/plugins/function.popup_init.php
@@ -21,21 +21,17 @@
  */
 function smarty_function_popup_init($params, &$smarty)
 {
-    static $already_init;
+    $zindex = 1000;
     
-    if (!$already_init) {
-        $zindex = 1000;
-        
-        if (!empty($params['zindex'])) {
-            $zindex = $params['zindex'];
-        }
-        
-        if (!empty($params['src'])) {
-            return '<div id="overDiv" style="position:absolute; visibility:hidden; z-index:'.$zindex.';"></div>' . "\n"
-             . '<script type="text/javascript" language="JavaScript" src="'.$params['src'].'"></script>' . "\n";
-        } else {
-            $smarty->trigger_error("popup_init: missing src parameter");
-        }
+    if (!empty($params['zindex'])) {
+        $zindex = $params['zindex'];
+    }
+    
+    if (!empty($params['src'])) {
+        return '<div id="overDiv" style="position:absolute; visibility:hidden; z-index:'.$zindex.';"></div>' . "\n"
+         . '<script type="text/javascript" language="JavaScript" src="'.$params['src'].'"></script>' . "\n";
+    } else {
+        $smarty->trigger_error("popup_init: missing src parameter");
     }
 }
 
