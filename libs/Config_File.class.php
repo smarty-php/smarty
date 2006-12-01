5c14c2c053a45adf59b45b2dbf65d2556f809f73
mohrt <mohrt@localhost>
2006-12-01 15:40:05 +0000

    update dev version numbers

--- libs/Config_File.class.php
+++ libs/Config_File.class.php
@@ -18,7 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * @link http://smarty.php.net/
- * @version 2.6.16-dev
+ * @version 2.6.17-dev
  * @copyright Copyright: 2001-2005 New Digital Group, Inc.
  * @author Andrei Zmievski <andrei@php.net>
  * @access public
