1fe6ad061c142b72535a42555d4b6ea757bc1116
monte.ohrt <monte.ohrt@localhost>
2008-09-22 19:26:32 +0000

    patch for security, php executed in templates

--- libs/Smarty_Compiler.class.php
+++ libs/Smarty_Compiler.class.php
@@ -1705,6 +1705,8 @@ class Smarty_Compiler extends Smarty {
         }
         // replace double quoted literal string with single quotes
         $_return = preg_replace('~^"([\s\w]+)"$~',"'\\1'",$_return);
+        // escape dollar sign if not printing a var
+        $_return = preg_replace('~\$(\W)~',"\\\\\$\\1",$_return);
         return $_return;
     }
 
