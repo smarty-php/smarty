0c2b18c7272fe85b70294327199e12686ff1c3e1
monte.ohrt <monte.ohrt@localhost>
2010-02-08 17:31:15 +0000

    move getTemplateVars() into data object

--- libs/sysplugins/smarty_internal_data.php
+++ libs/sysplugins/smarty_internal_data.php
@@ -159,6 +159,45 @@ class Smarty_Internal_Data {
     } 
 
     /**
+    * Returns a single or all template variables
+    * 
+    * @param string $varname variable name or null
+    * @return string variable value or or array of variables
+    */
+    function getTemplateVars($varname = null, $_ptr = null, $search_parents = true)
+    {
+        if (isset($varname)) {
+            $_var = $this->getVariable($varname, $_ptr, $search_parents);
+            if (is_object($_var)) {
+                return $_var->value;
+            } else {
+                return null;
+            } 
+        } else {
+            $_result = array();
+            if ($_ptr === null) {
+                $_ptr = $this;
+            } while ($_ptr !== null) {
+                foreach ($_ptr->tpl_vars AS $key => $var) {
+                    $_result[$key] = $var->value;
+                } 
+                // not found, try at parent
+                if ($search_parents) {
+                    $_ptr = $_ptr->parent;
+                } else {
+                    $_ptr = null;
+                } 
+            } 
+            if ($search_parents) {
+                foreach ($this->global_tpl_vars AS $key => $var) {
+                    $_result[$key] = $var->value;
+                } 
+            } 
+            return $_result;
+        } 
+    }
+    
+    /**
     * clear the given assigned template variable.
     * 
     * @param string $ |array $tpl_var the template variable(s) to clear
--- libs/sysplugins/smarty_internal_utility.php
+++ libs/sysplugins/smarty_internal_utility.php
@@ -154,45 +154,6 @@ class Smarty_Internal_Utility {
         return $_count;
     }
     
-    /**
-    * Returns a single or all template variables
-    * 
-    * @param string $varname variable name or null
-    * @return string variable value or or array of variables
-    */
-    function getTemplateVars($varname = null, $_ptr = null, $search_parents = true)
-    {
-        if (isset($varname)) {
-            $_var = $this->smarty->getVariable($varname, $_ptr, $search_parents);
-            if (is_object($_var)) {
-                return $_var->value;
-            } else {
-                return null;
-            } 
-        } else {
-            $_result = array();
-            if ($_ptr === null) {
-                $_ptr = $this->smarty;
-            } while ($_ptr !== null) {
-                foreach ($_ptr->tpl_vars AS $key => $var) {
-                    $_result[$key] = $var->value;
-                } 
-                // not found, try at parent
-                if ($search_parents) {
-                    $_ptr = $_ptr->parent;
-                } else {
-                    $_ptr = null;
-                } 
-            } 
-            if ($search_parents) {
-                foreach ($this->smarty->global_tpl_vars AS $key => $var) {
-                    $_result[$key] = $var->value;
-                } 
-            } 
-            return $_result;
-        } 
-    }
-    
     function testInstall()
     {
         echo "<PRE>\n";
