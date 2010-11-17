dd332db4558b5214ccdb5d52b54287998918a61d
uwe.tews@googlemail.com <uwe.tews@googlemail.com>
2010-11-17 23:15:12 +0000

    -bugfix in function.fetch.php, reference to undefined $smarty

--- libs/plugins/function.fetch.php
+++ libs/plugins/function.fetch.php
@@ -52,7 +52,7 @@ function smarty_function_fetch($params, $template)
                 $host = $server_name = $uri_parts['host'];
                 $timeout = 30;
                 $accept = "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, */*";
-                $agent = "Smarty Template Engine ".$smarty->_version;
+                $agent = "Smarty Template Engine ".$template->_version;
                 $referer = "";
                 $uri = !empty($uri_parts['path']) ? $uri_parts['path'] : '/';
                 $uri .= !empty($uri_parts['query']) ? '?' . $uri_parts['query'] : '';
