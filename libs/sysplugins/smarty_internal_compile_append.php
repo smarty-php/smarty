015f81725971f6025c4b7510a70af887133e7b37
uwe.tews@googlemail.com <uwe.tews@googlemail.com>
2010-12-28 22:40:13 +0000

    - bugfix multiple {append} tags failed to compile.

--- libs/sysplugins/smarty_internal_compile_append.php
+++ libs/sysplugins/smarty_internal_compile_append.php
@@ -13,10 +13,6 @@
  * Smarty Internal Plugin Compile Append Class
  */
 class Smarty_Internal_Compile_Append extends Smarty_Internal_Compile_Assign {
-	// attribute definitions
-    public $required_attributes = array('var', 'value');
-    public $shorttag_order = array('var', 'value');
-    public $optional_attributes = array('scope', 'index'); 
 
     /**
      * Compiles code for the {append} tag
@@ -29,6 +25,10 @@ class Smarty_Internal_Compile_Append extends Smarty_Internal_Compile_Assign {
     public function compile($args, $compiler, $parameter)
     {
         $this->compiler = $compiler;
+        // the following must be assigned at runtime because it will be overwritten in parent class
+        $this->required_attributes = array('var', 'value');
+        $this->shorttag_order = array('var', 'value');
+        $this->optional_attributes = array('scope','index');
         // check and get attributes
         $_attr = $this->_get_attributes($args); 
         // map to compile assign attributes
--- libs/sysplugins/smarty_internal_compile_assign.php
+++ libs/sysplugins/smarty_internal_compile_assign.php
@@ -25,6 +25,7 @@ class Smarty_Internal_Compile_Assign extends Smarty_Internal_CompileBase {
     public function compile($args, $compiler, $parameter)
     {
         $this->compiler = $compiler;
+        // the following must be assigned at runtime because it will be overwritten in Smarty_Internal_Compile_Append
         $this->required_attributes = array('var', 'value');
         $this->shorttag_order = array('var', 'value');
         $this->optional_attributes = array('scope');
