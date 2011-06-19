a55e05506e6db870df3839a92e738d19c0d0b867
uwe.tews@googlemail.com <uwe.tews@googlemail.com>
2011-06-19 20:34:28 +0000

    - bugfix clear_config Smarty2 BC wrapper function was missing

--- libs/sysplugins/smarty_internal_wrapper.php
+++ libs/sysplugins/smarty_internal_wrapper.php
@@ -4,29 +4,29 @@
  * Project:     Smarty: the PHP compiling template engine
  * File:        smarty_internal_wrapper.php
  * SVN:         $Id: $
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  * For questions, help, comments, discussion, etc., please join the
  * Smarty mailing list. Send a blank e-mail to
  * smarty-discussion-subscribe@googlegroups.com
- * 
+ *
  * @link http://www.smarty.net/
  * @copyright 2008 New Digital Group, Inc.
- * @author Monte Ohrt <monte at ohrt dot com> 
- * @author Uwe Tews 
+ * @author Monte Ohrt <monte at ohrt dot com>
+ * @author Uwe Tews
  * @package Smarty
  * @subpackage PluginsInternal
  * @version 3-SVN$Rev: 3286 $
@@ -37,17 +37,17 @@
  */
 
 class Smarty_Internal_Wrapper {
-  
+
     protected $smarty;
 
     function __construct($smarty) {
       $this->smarty = $smarty;
     }
-    
+
     /**
      * Converts smarty2-style function call to smarty 3-style function call
      * This is expensive, be sure to port your code to Smarty 3!
-     * 
+     *
      * @param string $name Smarty 2 function name
      * @param array $args Smarty 2 function args
      */
@@ -95,6 +95,8 @@ class Smarty_Internal_Wrapper {
                  return call_user_func_array(array($this->smarty,'clearAllCache'),$args);
               case 'cache':
                  return call_user_func_array(array($this->smarty,'clearCache'),$args);
+              case 'config':
+                 return call_user_func_array(array($this->smarty,'clearConfig'),$args);
               case 'compiled_template':
                  return call_user_func_array(array($this->smarty,'clearCompiledTemplate'),$args);
            }
