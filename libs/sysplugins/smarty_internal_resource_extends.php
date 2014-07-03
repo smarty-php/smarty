c2bba29216bba214734f98cfdd894d6699f2239c
Uwe.Tews@googlemail.com <Uwe.Tews@googlemail.com>
2014-07-03 23:16:23 +0000

    - bugfix the bufix of 02.06.2014 broke correct handling of child templates with same name but different template folders in extends resource (issue 194 and topic 25099)

--- libs/sysplugins/smarty_internal_resource_extends.php
+++ libs/sysplugins/smarty_internal_resource_extends.php
@@ -44,14 +44,14 @@ class Smarty_Internal_Resource_Extends extends Smarty_Resource
                 throw new SmartyException("Resource type {$s->type} cannot be used with the extends resource type");
             }
             $sources[$s->uid] = $s;
-            $uid .= $s->filepath;
+            $uid .= realpath($s->filepath);
             if ($_template && $_template->smarty->compile_check) {
                 $exists = $exists && $s->exists;
             }
         }
         $source->components = $sources;
         $source->filepath = $s->filepath;
-        $source->uid = sha1(realpath($uid));
+        $source->uid = sha1($uid);
         if ($_template && $_template->smarty->compile_check) {
             $source->timestamp = $s->timestamp;
             $source->exists = $exists;
