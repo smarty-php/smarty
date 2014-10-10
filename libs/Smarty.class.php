a1dac5a053e48a39c7f026fef07ddc6b485568fd
Uwe.Tews@googlemail.com <Uwe.Tews@googlemail.com>
2014-10-10 17:12:39 +0000

    Switch trunk version to 3.1.21-dev

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -110,7 +110,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = 'Smarty-3.1.19-dev';
+    const SMARTY_VERSION = 'Smarty-3.1.21-dev';
 
     /**
      * define variable scopes
