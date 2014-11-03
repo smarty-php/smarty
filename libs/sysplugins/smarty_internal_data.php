158b7be58a873cbc335f3de0a03778f9128cdf7b
Uwe Tews <uwe.tews@googlemail.com>
2014-11-03 22:27:32 +0100

    Debug Console Update
    - bugfix Debug Console did not show included subtemplates since 3.1.17
    (forum 25301)
    - bugfix Modifier debug_print_var did not limit recursion or prevent
    recursive object display at Debug Console
    (ATTENTION: parameter order has changed to be able to specify maximum
    recursion)
    - bugfix Debug consol did not include subtemplate information with
    $smarty->merge_compiled_includes = true
    - improvement The template variables are no longer displayed as objects
    on the Debug Console
    - improvement $smarty->createData($parent = null, $name = null) new
    optional name parameter for display at Debug Console
    - addition of some hooks for future extension of Debug Console

--- libs/sysplugins/smarty_internal_data.php
+++ libs/sysplugins/smarty_internal_data.php
@@ -455,6 +455,19 @@ class Smarty_Internal_Data
 class Smarty_Data extends Smarty_Internal_Data
 {
     /**
+     * Counter
+     *
+     * @var int
+     */
+    static $count = 0;
+
+    /**
+     * Data block name
+     *
+     * @var string
+     */
+    public $dataObjectName = '';
+    /**
      * Smarty object
      *
      * @var Smarty
@@ -466,11 +479,14 @@ class Smarty_Data extends Smarty_Internal_Data
      *
      * @param Smarty|array $_parent parent template
      * @param Smarty|Smarty_Internal_Template       $smarty  global smarty instance
+     * @param string $name optional data block name
      *
      * @throws SmartyException
      */
-    public function __construct($_parent = null, $smarty = null)
+    public function __construct($_parent = null, $smarty = null, $name = null)
     {
+        self::$count++;
+        $this->dataObjectName = 'Data_object ' . (isset($name) ? "'{$name}'" : self::$count);
         $this->smarty = $smarty;
         if (is_object($_parent)) {
             // when object set up back pointer
