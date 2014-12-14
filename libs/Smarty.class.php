2d39248aa4f4fd7001037dd17e4faca9281fb402
Uwe Tews <uwe.tews@googlemail.com>
2014-12-14 05:49:09 +0100

    fix version

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -113,7 +113,7 @@ class Smarty extends Smarty_Internal_TemplateBase {
     /**
      * smarty version
      */
-    const SMARTY_VERSION = 'Smarty-3.1-DEV';
+    const SMARTY_VERSION = 'Smarty-3.1.11';
 
     /**
      * define variable scopes
