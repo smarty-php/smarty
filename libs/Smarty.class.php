07c781d9cf1451aa9fb6e005a037ea73241f6237
Uwe Tews <uwe.tews@googlemail.com>
2015-05-13 00:33:23 +0200

    - bugfix in nested {foreach} saved item attributes got overwritten https://github.com/smarty-php/smarty/issues/33

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -111,7 +111,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.24-dev/1';
+    const SMARTY_VERSION = '3.1.24-dev/2';
 
     /**
      * define variable scopes
--- libs/sysplugins/smarty_internal_compile_foreach.php
+++ libs/sysplugins/smarty_internal_compile_foreach.php
@@ -208,7 +208,8 @@ class Smarty_Internal_Compile_Foreach extends Smarty_Internal_CompileBase
                 }
             }
         }
-        $output .= "\$foreachItemSav = \$_smarty_tpl->tpl_vars[$item];\n";
+        $itemName = trim($item,"'\"");
+        $output .= "\$foreach_{$itemName}_Sav = \$_smarty_tpl->tpl_vars[$item];\n";
         $output .= "?>";
 
         return $output;
@@ -239,8 +240,9 @@ class Smarty_Internal_Compile_Foreachelse extends Smarty_Internal_CompileBase
 
         list($openTag, $nocache, $item, $key, $foo) = $this->closeTag($compiler, array('foreach'));
         $this->openTag($compiler, 'foreachelse', array('foreachelse', $nocache, $item, $key, false));
+        $itemName = trim($item,"'\"");
         $output = "<?php\n";
-        $output .= "\$_smarty_tpl->tpl_vars[$item] = \$foreachItemSav;\n";
+        $output .= "\$_smarty_tpl->tpl_vars[$item] = \$foreach_{$itemName}_Sav;\n";
         $output .= "}\n";
         $output .= "if (!\$_smarty_tpl->tpl_vars[$item]->_loop) {\n?>";
         return $output;
@@ -274,9 +276,10 @@ class Smarty_Internal_Compile_Foreachclose extends Smarty_Internal_CompileBase
         }
 
         list($openTag, $compiler->nocache, $item, $key, $restore) = $this->closeTag($compiler, array('foreach', 'foreachelse'));
+        $itemName = trim($item,"'\"");
         $output = "<?php\n";
         if ($restore) {
-            $output .= "\$_smarty_tpl->tpl_vars[$item] = \$foreachItemSav;\n";
+            $output .= "\$_smarty_tpl->tpl_vars[$item] = \$foreach_{$itemName}_Sav;\n";
         }
         $output .= "}\n?>";
 
