04f0090c40b1a0e1fb55dbe0297f08386b36de21
Uwe Tews <uwe.tews@googlemail.com>
2015-05-12 04:08:22 +0200

    release 3.1.23

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -111,7 +111,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.23-dev/0';
+    const SMARTY_VERSION = '3.1.23';
 
     /**
      * define variable scopes
