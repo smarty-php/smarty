e3123c04273b26a354efbf7aa4dd41bc4bb0af6b
Uwe Tews <uwe.tews@googlemail.com>
2015-05-16 16:33:50 +0200

    - improvement higher compilation speed by modified lexer/parser generator at "smarty/smarty-lexer"

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -111,7 +111,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.24-dev/5';
+    const SMARTY_VERSION = '3.1.24-dev/6';
 
     /**
      * define variable scopes
