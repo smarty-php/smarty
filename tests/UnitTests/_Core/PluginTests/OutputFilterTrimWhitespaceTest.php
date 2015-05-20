47b6fab3d9f110a8279713479f9ce0fc12601872
Uwe Tews <uwe.tews@googlemail.com>
2015-05-20 05:05:28 +0200

    fix result

--- tests/UnitTests/_Core/PluginTests/OutputFilterTrimWhitespaceTest.php
+++ tests/UnitTests/_Core/PluginTests/OutputFilterTrimWhitespaceTest.php
@@ -22,7 +22,7 @@ class OutputFilterTrimWhitespaceTest extends PHPUnit_Smarty
     public function testWhitespace()
     {
         $expected =
-'<!DOCTYPE html> <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de"> <head> <meta charset="utf-8"/> <meta http-equiv="content-type" content="text/html; charset=utf-8"/> <title>whitespace</title> <meta name="title" content=""/> <meta name="description" content=""/> <link rel="stylesheet" type="text/css" href="screen.css"/> </head> <body> <!--[if lte IE 6]>internet explorer conditional comment<![endif]--> <!--[if lte IE 7]>internet explorer conditional comment<![endif]--> <div class=" asdasd   " id=\'not\' data-one=" " style=" " title=\' \'></div> <img src="foo" alt=""/> <script type="text/javascript">
+'<!DOCTYPE html> <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de"> <head> <meta charset="utf-8"/> <meta http-equiv="content-type" content="text/html; charset=utf-8"/> <title>whitespace</title> <meta name="title" content=""/> <meta name="description" content=""/> <link rel="stylesheet" type="text/css" href="screen.css"/> </head> <body> <!--[if lte IE 6]>internet explorer conditional comment<![endif]--> <!--[if lte IE 7]>internet explorer conditional comment<![endif]--> <div class="  asdasd   " id=\'not\' data-one=" " style=" " title=\' \'></div> <img src="foo" alt=""/> <script type="text/javascript">
     foobar
 </script> <script>
     foobar
