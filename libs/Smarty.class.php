531b29888681c1a0918858c7c0da89dc64552f6a
Uwe Tews <uwe.tews@googlemail.com>
2015-05-23 19:25:50 +0200

    Prepare 3.1.25-dev

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -111,7 +111,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.24';
+    const SMARTY_VERSION = '3.1.25-dev/0';
 
     /**
      * define variable scopes
