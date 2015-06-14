83f3f6d7f151b30a74e59eae8349aa1b2c8670ac
Uwe Tews <uwe.tews@googlemail.com>
2015-06-14 04:23:50 +0200

    - bugfix an output tag with variable, modifier followed by an operator like {$foo|modifier+1} did fail https://github.com/smarty-php/smarty/issues/53

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -111,7 +111,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.25-dev/7';
+    const SMARTY_VERSION = '3.1.25-dev/8';
 
     /**
      * define variable scopes
