ec383370cb1a800e824c17bea0625edd83cf6908
Uwe Tews <uwe.tews@googlemail.com>
2015-06-15 23:39:42 +0200

    - optimization of smarty_cachereource_keyvaluestore.php code

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -111,7 +111,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.25-dev/8';
+    const SMARTY_VERSION = '3.1.25-dev/9';
 
     /**
      * define variable scopes
