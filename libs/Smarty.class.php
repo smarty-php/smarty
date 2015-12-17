fb80bd53a56396d5da0c9461f93154c554638df9
uwetews <uwe.tews@googlemail.com>
2015-12-17 20:31:30 +0100

    - bugfix {$smarty.capture.nameFail} did lowercase capture name https://github.com/smarty-php/smarty/issues/135

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -118,7 +118,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.29-dev/7';
+    const SMARTY_VERSION = '3.1.29-dev/8';
 
     /**
      * define variable scopes
