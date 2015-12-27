4dd8b0bf7346d67aca762e8e841ded6e95713717
uwetews <uwe.tews@googlemail.com>
2015-12-27 05:43:11 +0100

    new _clearTemplateCache() method

--- libs/sysplugins/smarty_internal_method_compilealltemplates.php
+++ libs/sysplugins/smarty_internal_method_compilealltemplates.php
@@ -98,7 +98,7 @@ class Smarty_Internal_Method_CompileAllTemplates
                 }
                 // free memory
                 unset($_tpl);
-                $_smarty->_cache['template_objects'] = array();
+                $_smarty->_clearTemplateCache();
                 if ($max_errors !== null && $_error_count == $max_errors) {
                     echo "\n<br><br>too many errors\n";
                     exit();
