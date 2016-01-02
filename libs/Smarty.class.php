418492b40bc79d214093991e4c149a2e5b987a23
uwetews <uwe.tews@googlemail.com>
2016-01-02 11:27:44 +0100

    - optimize block plugin compiler

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -121,7 +121,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.30-dev/16';
+    const SMARTY_VERSION = '3.1.30-dev/17';
 
     /**
      * define variable scopes
