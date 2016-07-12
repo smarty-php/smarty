bfca72e71b9ea8b64be825a0c96db8c33df1677d
uwetews <uwe.tews@googlemail.com>
2016-07-12 23:01:29 +0200

    update compiler plugin test

--- tests/UnitTests/Compiler/CompilerPlugin/CompileCompilerPluginTest.php
+++ tests/UnitTests/Compiler/CompilerPlugin/CompileCompilerPluginTest.php
@@ -28,15 +28,44 @@ class CompileCompilerPluginTest extends PHPUnit_Smarty
     /**
      * test compiler plugin tag in template file
      */
-    public function testCompilerPlugin()
+    public function testCompilerPluginFunction()
     {
         $this->smarty->registerPlugin(Smarty::PLUGIN_COMPILER, 'compilerplugin', 'mycompilerplugin');
-        $tpl = $this->smarty->createTemplate('compilerplugintest.tpl');
-        $this->assertEquals("Hello World", $this->smarty->fetch($tpl));
+        $this->smarty->compile_id = 'function';
+        $this->assertEquals("Hello World", $this->smarty->fetch('compilerplugintest.tpl'));
+    }
+    /**
+     * test compiler plugin tag in template file
+     */
+    public function testCompilerPluginClassStatic()
+    {
+        $this->smarty->registerPlugin(Smarty::PLUGIN_COMPILER, 'compilerplugin', array('CompilerPluginClass', 'statCompile'));
+        $this->smarty->compile_id = 'static';
+        $this->assertEquals("Static World", $this->smarty->fetch('compilerplugintest.tpl'));
+    }
+    /**
+     * test compiler plugin tag in template file
+     */
+    public function testCompilerPluginClassObject()
+    {
+        $plugin = new CompilerPluginClass;
+        $this->smarty->registerPlugin(Smarty::PLUGIN_COMPILER, 'compilerplugin', array($plugin, 'compile'));
+        $this->smarty->compile_id = 'object';
+        $this->assertEquals("Public World", $this->smarty->fetch('compilerplugintest.tpl'));
     }
 }
 
 function mycompilerplugin($params, $compiler)
 {
     return '<?php echo \'Hello World\';?>';
+}
+
+class CompilerPluginClass
+{
+    static function statCompile ($params, $compiler) {
+        return '<?php echo \'Static World\';?>';
+    }
+    public function compile ($params, $compiler) {
+        return '<?php echo \'Public World\';?>';
+    }
 }
