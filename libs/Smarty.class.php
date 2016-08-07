7f932e7ac4d409ebcaae418c22755a3d6fa1a333
uwetews <uwe.tews@googlemail.com>
2016-08-07 20:57:11 +0200

    Prepare 3.1.31-dev

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -27,7 +27,7 @@
  * @author    Uwe Tews
  * @author    Rodney Rehm
  * @package   Smarty
- * @version   3.1.30
+ * @version   3.1.31-dev
  */
 
 /**
@@ -121,7 +121,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.30';
+    const SMARTY_VERSION = '3.1.31-dev/1';
 
     /**
      * define variable scopes
