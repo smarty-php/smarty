61b59a9ec3d736b3473cb644e3e24e9dc14bc31a
uwetews <uwe.tews@googlemail.com>
2017-05-21 02:23:53 +0200

    - bugfix remove special treatment of classes implementing ArrayAccess in {foreach}     https://github.com/smarty-php/smarty/issues/332

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -108,7 +108,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.32-dev-3';
+    const SMARTY_VERSION = '3.1.32-dev-5';
 
     /**
      * define variable scopes
