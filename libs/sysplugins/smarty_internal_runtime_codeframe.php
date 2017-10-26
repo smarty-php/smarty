87f260e3a9a4d550e90dfa1b07ae94d8b6ab5053
Uwe Tews <uwe.tews@googlemail.com>
2017-10-26 04:37:39 +0200

    fix Smarty version in header comment (reverted from commit 5ec2e1f62bb1eef317ee6f935b96f47526680823)

--- libs/sysplugins/smarty_internal_runtime_codeframe.php
+++ libs/sysplugins/smarty_internal_runtime_codeframe.php
@@ -41,7 +41,7 @@ class Smarty_Internal_Runtime_CodeFrame
             $properties[ 'cache_lifetime' ] = $_template->cache_lifetime;
         }
         $output = "<?php\n";
-        $output .= "/* Smarty version {$properties[ 'version' ]}, created on " . strftime("%Y-%m-%d %H:%M:%S") .
+        $output .= "/* Smarty version {Smarty::SMARTY_VERSION}, created on " . strftime("%Y-%m-%d %H:%M:%S") .
                    "\n  from \"" . str_replace('*/','* /',$_template->source->filepath) . "\" */\n\n";
         $output .= "/* @var Smarty_Internal_Template \$_smarty_tpl */\n";
         $dec = "\$_smarty_tpl->_decodeProperties(\$_smarty_tpl, " . var_export($properties, true) . ',' .
