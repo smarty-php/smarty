6bff8be955ab40eba5a2aa5ec26f5579818e2fd7
Chad Meyers <chad@nobodyfamous.ca>
2018-07-17 15:05:48 -0300

    Update function.html_radios.php
    only if not empty

--- libs/plugins/function.html_radios.php
+++ libs/plugins/function.html_radios.php
@@ -159,7 +159,9 @@ function smarty_function_html_radios($params, Smarty_Internal_Template $template
     }
 
     // remove 'separator' from last index
-    $_html_result[(count($_html_result) - 1)] = substr($_html_result[(count($_html_result) - 1)],0,-(strlen($separator)));
+    if (!empty($separator)) {
+      $_html_result[(count($_html_result) - 1)] = substr($_html_result[(count($_html_result) - 1)],0,-(strlen($separator)));
+    }
 
     if (!empty($params[ 'assign' ])) {
         $template->assign($params[ 'assign' ], $_html_result);
