5983f97dc92320ce3ee2d1895b25659377f2bfff
Simon Wisselink <s.wisselink@iwink.nl>
2021-01-15 15:24:52 +0100

    Fixed unit tests for removed deprecated code in previous commit

--- tests/UnitTests/ResourceTests/ResourcePlugins/PHPunitplugins/resource.db.php
+++ tests/UnitTests/ResourceTests/ResourcePlugins/PHPunitplugins/resource.db.php
@@ -15,12 +15,8 @@ class Smarty_Resource_Db extends Smarty_Resource_Recompiled {
 	public function populate(Smarty_Template_Source $source, Smarty_Internal_Template $_template = null) {
 		$source->filepath = 'db:';
 		$source->uid = sha1($source->resource);
-		$source->timestamp = 0;
+		$source->timestamp = 1000000000;
 		$source->exists = true;
-	}
-
-	public function populateTimestamp(Smarty_Template_Source $source): int {
-		return 1000000000;
 	}
 
 	public function getContent(Smarty_Template_Source $source) {
