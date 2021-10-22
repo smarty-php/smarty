22329d9d30d4fc19b581b178abc583ace4454dd2
Simon Wisselink <s.wisselink@iwink.nl>
2021-10-22 16:42:17 +0200

    removed debug code

--- make-release.sh
+++ make-release.sh
@@ -11,18 +11,16 @@ git checkout -b "release/$1"
 sed -i "s/=== Unreleased ===/=== Unreleased ===\\n\\n=== $1 - $(date +%Y-%m-%d) ===/" ChangeLog
 sed -i "s/var \$_version = '[^']\+';/var \$_version = '$1';/" libs/Smarty.class.php
 
-exit;
+git add ChangeLog libs/Smarty.class.php
+git commit -m "version bump"
 
-#git add ChangeLog libs/Smarty.class.php
-#git commit -m "version bump"
-#
-#git checkout support/2.6
-#git pull
-#git merge --no-ff "release/$1"
-#git branch -d "release/$1"
-#git tag -a "v$1" -m "Release $1"
-#
-#printf 'Done creating release %s\n' "$1"
-#
-## shellcheck disable=SC2016
-#printf 'Run `git push --follow-tags origin` to publish it.\n'
+git checkout support/2.6
+git pull
+git merge --no-ff "release/$1"
+git branch -d "release/$1"
+git tag -a "v$1" -m "Release $1"
+
+printf 'Done creating release %s\n' "$1"
+
+# shellcheck disable=SC2016
+printf 'Run `git push --follow-tags origin` to publish it.\n'
