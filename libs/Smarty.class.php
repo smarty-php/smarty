e603269eda871a2ba33a563aa0b2ea3d43734ed3
Simon Wisselink <s.wisselink@iwink.nl>
2021-11-25 22:18:24 +0100

    version bump

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -98,7 +98,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '4.0.0-rc.0';
+    const SMARTY_VERSION = '4.0.0';
     /**
      * define variable scopes
      */
