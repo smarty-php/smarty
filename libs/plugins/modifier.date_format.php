fc06d246bfe16b8d3ef94e2583e6518b4448c23f
Ian <aristophian@googlemail.com>
2022-08-08 18:09:33 +0200

    Temporary fix strftime deprecation notes up from PHP 8.1+
    Convert to DateTime Interface with intl before PHP 9.

--- libs/plugins/modifier.date_format.php
+++ libs/plugins/modifier.date_format.php
@@ -79,7 +79,7 @@ function smarty_modifier_date_format($string, $format = null, $default_date = ''
             }
             $format = str_replace($_win_from, $_win_to, $format);
         }
-        return strftime($format, $timestamp);
+        return @strftime($format, $timestamp);
     } else {
         return date($format, $timestamp);
     }
