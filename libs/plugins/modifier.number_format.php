9998393c1f59712a449259aaec5cfe92b9d542c6
cmelius <christianmelius@gmail.com>
2022-08-24 15:25:25 +0200

    fix number_format

---
+++ libs/plugins/modifier.number_format.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Smarty plugin
+ *
+ * @package    Smarty
+ * @subpackage PluginsModifier
+ */
+/**
+ * Smarty number_format modifier plugin
+ * Type:     modifier
+ * Name:     number-format
+ * Purpose:  php number_format wrapper to fix PHP 8.1 "Passing null to parameter #1 ($num) of type float is deprecated"
+ *
+ * @author Christian Meius <https://github.com/cmelius>
+ *
+ * @param string $num number to format
+ * @param string $decimals  number of decimals
+ * @param string $decimal_separator
+ * @param string $thousands_separator
+ *
+ * @return string
+ */
+function smarty_modifier_number_format($num, $decimals = 0, $decimal_separator =".", $thousands_separator = ",")
+{
+    static $is_loaded = false;
+
+    $num = null !== $num ? $num : 0;
+
+    return number_format($num, $decimals, $decimal_separator, $thousands_separator);
+}
