b02061878fecae084aee41e452a2f1bfe001be6f
Simon Wisselink <s.wisselink@iwink.nl>
2023-01-13 23:28:18 +0100

    Update test runners: no longer support PHP7.1, add PHP8.2

--- run-tests-for-all-php-versions.sh
+++ run-tests-for-all-php-versions.sh
@@ -5,9 +5,9 @@
 # - ./run-tests-for-all-php-versions.sh --group 20221124
 # - ./run-tests-for-all-php-versions.sh --exclude-group slow
 
-docker-compose run php71 ./run-tests.sh $@ && \
 docker-compose run php72 ./run-tests.sh $@ && \
 docker-compose run php73 ./run-tests.sh $@ && \
 docker-compose run php74 ./run-tests.sh $@ && \
 docker-compose run php80 ./run-tests.sh $@ && \
-docker-compose run php81 ./run-tests.sh $@
+docker-compose run php81 ./run-tests.sh $@ && \
+docker-compose run php82 ./run-tests.sh $@
