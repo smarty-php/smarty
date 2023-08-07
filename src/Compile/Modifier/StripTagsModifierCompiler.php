3750c644a5fdcb3ba90d5b4d70a3dc9a05773cd8
Simon Wisselink <s.wisselink@iwink.nl>
2023-08-07 23:49:24 +0200

    Missed part of commit a3cbdc46 in the merge

--- src/Compile/Modifier/StripTagsModifierCompiler.php
+++ src/Compile/Modifier/StripTagsModifierCompiler.php
@@ -14,7 +14,7 @@ class StripTagsModifierCompiler extends Base {
 
 	public function compile($params, \Smarty\Compiler\Template $compiler) {
 		if (!isset($params[ 1 ]) || $params[ 1 ] === true || trim($params[ 1 ], '"') === 'true') {
-			return "preg_replace('!<[^>]*?>!', ' ', {$params[0]} ?: '')";
+			return "preg_replace('!<[^>]*?>!', ' ', (string) {$params[0]})";
 		} else {
 			return 'strip_tags((string) ' . $params[ 0 ] . ')';
 		}
